34bf20dcbc60284444bd136054f6ebb7bffeb003
student <mateuszpavvlovvicz@gmail.com>
2019-10-05 11:28:47 +0200

    lesson 1

---
+++ podstawy programowania/1._first project/1._first project/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _1._first_project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Imię:Abert");//wyświetli imię
+            Console.WriteLine("#\n##\n###");
+            string name;
+            name = "anna";
+
+            Console.WriteLine(name);
+            Console.WriteLine("masz na imię{0}", name);
+            int age = 20;
+            Console.WriteLine("masz na imię:{0}, wiek{20}", name, age); 
+            sbyte
+                
+            Console.ReadKey();
+            
+
+        }
+    }
+}
